660adf201a561304a4aff141a6404b8fd6332187
nicheelee24 <nicheelee@gmail.com>
2025-02-24 16:58:19 +0530

    env

--- routes/api/pay.js
+++ routes/api/pay.js
@@ -37,6 +37,7 @@ router.post("/deposit_dct",auth, async (req, res) => {
         const brand_uid= user.name;
         const dct_key=process.env.KEY_ID;
         const HASH = brand_id+brand_uid+dct_key;
+		console.log(HASH);
         const hashh = require('crypto').createHash('md5').update(HASH).digest('hex').toString().toUpperCase();
         const DEPOSIT_URL = `${process.env.PMG_BASE_URL}/api/v1/Payment/Deposit`;
         const DEPOSIT_URL_DCT = `${process.env.DCT_BASE_URL}/dct/credit`;
